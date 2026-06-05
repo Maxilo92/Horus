e79eaf5ce39689215592aa6128edc4556013ac1f
Maxilo92 <Priesnitz.maximilian@icloud.com>
2026-06-05 19:32:38 +0200

    Update .gitignore and remove build artifacts from index

--- build3/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/maximilian/Documents/Code/Tactileviewer/Project_Horus/build3/_deps/googletest-src/googletest
-# Build directory: /Users/maximilian/Documents/Code/Tactileviewer/Project_Horus/build3/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
